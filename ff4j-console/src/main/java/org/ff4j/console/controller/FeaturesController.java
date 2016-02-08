9711eb16ab5ad88ea7c857304e8d3c90059a355b
parkerj <parkerj@qualtrics.com>
2016-02-08 13:46:36 -0700

    oops

--- ff4j-console/src/main/java/org/ff4j/console/controller/FeaturesController.java
+++ ff4j-console/src/main/java/org/ff4j/console/controller/FeaturesController.java
@@ -22,11 +22,7 @@ package org.ff4j.console.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 import javax.servlet.ServletOutputStream;
